e6b19d3a979d38122106fc868f6927a33deab153
Aashutosh <ashutoshg1101@gmail.com>
2023-08-03 00:38:37 +0530

    commit no. 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import { useState } from 'react';
+
+
+let userData;
 
 function App() {
+  const [data,setdata] = useState([]); 
+  const [query,setquery] = useState("");
+
+  axios
+  .get("https://jsonplaceholder.typicode.com/users")
+  .then(data => setdata(data.data))
+  .catch(error => console.log(error));
+
+  let temp = data.filter((user)=>user.id.toString().includes(query)||user.name.toLowerCase().includes(query)||user.username.toLowerCase().includes(query)||user.email.toLowerCase().includes(query));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type='text' placeholder='search...' className='search' onChange={(e) => setquery(e.target.value)}/>
+      <div style={{overflowX: 'auto'}}>
+            <table>
+                <tr>
+                    <th>id</th>
+                    <th>Name</th>
+                    <th>UserName</th>
+                    <th>email</th>
+                </tr>
+                {temp.map((val, key) => {
+                    return (
+                      <tr key={key}>
+                          <td>{val.id}</td>
+                          <td>{val.name}</td>
+                          <td>{val.username}</td>
+                          <td>{val.email}</td>
+                      </tr>
+                  )
+              })} 
+            </table>  
+      </div>    
     </div>
   );
 }
+
 
 export default App;
